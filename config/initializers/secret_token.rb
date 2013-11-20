86a5865d4fa0dba0029e9c4baad16d2b9f7d2dc6
Sandy Petkanics <spetkanics@gmail.com>
2013-11-20 10:38:10 -0500

    all changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Almostsixty::Application.config.secret_key_base = '5b5a14b773dee94dfafe4d6451e2c922e991914a5daa2f6cb766d3e0856422af1fab0dc8101934c98167d84aa0a8fe68f72f15c6832c2a8510e74bae0c37658d'
